216b19a7b8a26c5eba4e04fa13dd8be905598597
Lorens Kockum <lorens.kockum@schibsted.com>
2017-01-09 15:35:05 +0100

    Eliminated loop in audit_6.2.15.sh
    The need is to list groups that are used but not defined. The old logic does a grep in /etc/group for every user. This is inefficent if there are many users. The new logic lists all the used groups, all the defined groups, and compares the lists.
    
    I have seen /etc/passwd and /etc/group files with comments in them, but the old logic wouldn't handle them any better. One could pre-filter out comments.

--- files/audit_6.2.15.sh
+++ files/audit_6.2.15.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
  
-for i in $(cut -s -d: -f4 /etc/passwd | sort -u ); do
-  grep -q -P "^.*?:[^:]*:$i:" /etc/group
-  if [ $? -ne 0 ]; then
-    echo "Group $i is referenced by /etc/passwd but does not exist in /etc/group"
-  fi
+comm -23 <(cut -s -d: -f4 /etc/passwd | sort -u) <(cut -s -d: -f3 /etc/group | sort -u) | while read GROUP ; do
+  echo "Group $GROUP is referenced by /etc/passwd but does not exist in /etc/group"
 done
