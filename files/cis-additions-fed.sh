605d6e456e20e7bc97fc8e28c748f60e87b568d5
Prasiddha01 <Prasiddha.bista@versent.com.au>
2019-08-05 11:02:10 +1000

    Initial Commit

---
+++ files/cis-additions-fed.sh
@@ -0,0 +1,149 @@
+#!/bin/bash
+set -x
+set -e
+set -u
+
+echo "============================"
+echo "     SCRIPT START"
+echo "============================"
+
+###############################################################################
+
+#5.3.2
+for i in /etc/pam.d/system-auth /etc/pam.d/password-auth; do
+sed -i '/auth\s\+required\s\+pam_deny.so/a\
+auth        required pam_faillock.so preauth audit silent deny=5 unlock_time=900' $i
+sed -i '/auth\s\+required\s\+pam_deny.so/a\
+auth        [success=1 default=bad] pam_unix.so' $i
+sed -i '/auth\s\+required\s\+pam_deny.so/a\
+auth        sufficient    pam_faillock.so authsucc audit deny=5 unlock_time=900' $i
+sed -i '/auth\s\+required\s\+pam_deny.so/a\
+auth        [default=die] pam_faillock.so authfail audit deny=5 unlock_time=900' $i
+done
+
+#5.3.3
+for i in /etc/pam.d/system-auth /etc/pam.d/password-auth; do
+sed -i 's/password\s\+sufficient\s\+pam_unix.so try_first_pass use_authtok nullok sha512 shadow/\
+password    sufficient    pam_unix.so remember=5 try_first_pass use_authtok nullok sha512 shadow/g' $i
+sed -i '/password\s\+required\s\+pam_deny.so/a\
+password    required      pam_pwhistory.so remember=5' $i
+done
+
+#5.4.4 Ensure default user umask is 027 or more restrictive
+sed -e '/umask 027/d' -i /etc/profile
+echo 'umask 027' >> /etc/profile
+
+#6.1.5 Ensure permissions on /etc/gshadow are configured
+chown root:root /etc/gshadow
+chmod 000 /etc/gshadow
+
+#6.1.9 Ensure permissions on /etc/gshadow- are configured
+chown root:root /etc/gshadow-
+chmod 600 /etc/gshadow-
+
+#  install package to provide better iptables functionality
+yum install -y iptables-services
+
+#3.5.2.1 Ensure IPv6 default deny firewall policy
+ip6tables -P INPUT DROP
+ip6tables -P OUTPUT DROP
+ip6tables -P FORWARD DROP
+
+#3.5.2.2 Ensure IPv6 loopback traffic is configured
+############Executes but doesn't return successful
+ip6tables -A INPUT -i lo -j ACCEPT
+ip6tables -A OUTPUT -o lo -j ACCEPT
+ip6tables -A INPUT -s ::1 -j DROP
+
+#4.2.4
+find /var/log -type f -exec chmod g-wx,o-rwx {} +
+chown root:root /var/log/wtmp
+chown root:root /var/log/btmp
+
+# 4.2.4 Helper
+chmod g-wx,o-rwx /var/log/wtmp
+chmod g-wx,o-rwx /var/log/cloud-init.log
+chmod g-wx,o-rwx /var/log/dmesg
+
+#3.5.1.2 Ensure loopback traffic is configured
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
+iptables -A INPUT -s 127.0.0.0/8 -j DROP
+
+# ALLOW ALL CONNECTIONS FOR IPV4 IPTABLES
+iptables -P INPUT ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -P FORWARD ACCEPT
+
+#  v4
+service iptables save
+systemctl enable iptables
+systemctl stop iptables
+systemctl start iptables
+
+#  v6
+service ip6tables save
+systemctl enable ip6tables
+systemctl stop ip6tables
+systemctl start ip6tables
+
+# 5.4.4 Ensure default user umask is 027 or more restrictive
+sed -e '/umask 027/d' -i /etc/profile
+echo 'umask 027' >> /etc/profile
+
+# 5.2.15 Ensure that strong Key Exchange algorithms are used
+echo "KexAlgorithms curve25519-sha256,curve25519-sha256@libssh.org,diffie-hellman-group14-sha256,diffie-hellman-group16-sha512,diffie-hellman-group18-sha512,ecdh-sha2-nistp521,ecdh-sha2-nistp384,ecdh-sha2-nistp256,diffie-hellman-group-exchangesha256" >> /etc/ssh/sshd_config
+
+# 4.1.3, 4.1.4, 4.1.5, 4.1.6, 4.1.7, 4.1.8, 4.1.9, 4.1.10, 4.1.11, 4.1.14, 4.1.15, 4.1.16, 4.1.17, 4.1.18
+echo "-w /var/log/lastlog -p wa -k logins" >> /etc/audit/rules.d/audit.rules
+echo "-w /var/run/faillock/ -p wa -k logins" >> /etc/audit/rules.d/audit.rules
+echo "-a always,exit -F arch=b64 -S unlink -S unlinkat -S rename -S renameat -F auid>=1000 -F auid!=4294967295 -k delete" >> /etc/audit/rules.d/audit.rules
+echo "-a always,exit -F arch=b32 -S unlink -S unlinkat -S rename -S renameat -F auid>=1000 -F auid!=4294967295 -k delete" >> /etc/audit/rules.d/audit.rules
+echo "-w /sbin/insmod -p x -k modules" >> /etc/audit/rules.d/audit.rules
+echo "-w /sbin/rmmod -p x -k modules" >> /etc/audit/rules.d/audit.rules
+echo "-w /sbin/modprobe -p x -k modules" >> /etc/audit/rules.d/audit.rules
+echo "-a always,exit -F arch=b64 -S init_module -S delete_module -k modules" >> /etc/audit/rules.d/audit.rules
+echo "-w /var/log/sudo.log -p wa -k actions" >> /etc/audit/rules.d/audit.rules
+echo "-e 2" >> /etc/audit/rules.d/audit.rules
+echo "-w /etc/sudoers -p wa -k scope" >> /etc/audit/rules.d/audit.rules
+echo "-w /etc/sudoers.d/ -p wa -k scope" >> /etc/audit/rules.d/audit.rules
+echo "-w /var/log/sudo.log -p wa -k actions" >> /etc/audit/rules.d/audit.rules
+echo "-a always,exit -F arch=b64 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts" >> /etc/audit/rules.d/audit.rules
+echo "-a always,exit -F arch=b32 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts" >> /etc/audit/rules.d/audit.rules
+echo "-w /etc/selinux/ -p wa -k MAC-policy" >> /etc/audit/rules.d/audit.rules
+echo "-w /usr/share/selinux/ -p wa -k MAC-policy" >> /etc/audit/rules.d/audit.rules
+echo "-a always,exit -F arch=b64 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EACCES -F auid>=1000 -F auid!=4294967295 -k access" >> /etc/audit/rules.d/audit.rules
+echo "-a always,exit -F arch=b32 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EACCES -F auid>=1000 -F auid!=4294967295 -k access" >> /etc/audit/rules.d/audit.rules
+echo "-a always,exit -F arch=b64 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EPERM -F auid>=1000 -F auid!=4294967295 -k access" >> /etc/audit/rules.d/audit.rules
+echo "-a always,exit -F arch=b32 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EPERM -F auid>=1000 -F auid!=4294967295 -k access" >> /etc/audit/rules.d/audit.rules
+echo "-a always,exit -F arch=b64 -S chmod -S fchmod -S fchmodat -F auid>=1000 -F auid!=4294967295 -k perm_mod" >> /etc/audit/rules.d/audit.rules
+echo "-a always,exit -F arch=b32 -S chmod -S fchmod -S fchmodat -F auid>=1000 -F auid!=4294967295 -k perm_mod" >> /etc/audit/rules.d/audit.rules
+echo "-a always,exit -F arch=b64 -S chown -S fchown -S fchownat -S lchown -F auid>=1000 -F auid!=4294967295 -k perm_mod" >> /etc/audit/rules.d/audit.rules
+echo "-a always,exit -F arch=b32 -S chown -S fchown -S fchownat -S lchown -F auid>=1000 -F auid!=4294967295 -k perm_mod" >> /etc/audit/rules.d/audit.rules
+echo "-a always,exit -F arch=b64 -S setxattr -S lsetxattr -S fsetxattr -S removexattr -S lremovexattr -S fremovexattr -F auid>=1000 -F auid!=4294967295 -k perm_mod" >> /etc/audit/rules.d/audit.rules
+echo "-a always,exit -F arch=b32 -S setxattr -S lsetxattr -S fsetxattr -S removexattr -S lremovexattr -S fremovexattr -F auid>=1000 -F auid!=4294967295 -k perm_mod" >> /etc/audit/rules.d/audit.rules
+echo "-w /var/run/utmp -p wa -k session" >> /etc/audit/rules.d/audit.rules
+echo "-w /var/log/wtmp -p wa -k logins" >> /etc/audit/rules.d/audit.rules
+echo "-w /var/log/btmp -p wa -k logins" >> /etc/audit/rules.d/audit.rules
+echo "-a always,exit -F arch=b64 -S sethostname -S setdomainname -k system-locale" >> /etc/audit/rules.d/audit.rules
+echo "-a always,exit -F arch=b32 -S sethostname -S setdomainname -k system-locale" >> /etc/audit/rules.d/audit.rules
+echo "-w /etc/issue -p wa -k system-locale" >> /etc/audit/rules.d/audit.rules
+echo "-w /etc/issue.net -p wa -k system-locale" >> /etc/audit/rules.d/audit.rules
+echo "-w /etc/hosts -p wa -k system-locale" >> /etc/audit/rules.d/audit.rules
+echo "-w /etc/sysconfig/network -p wa -k system-locale" >> /etc/audit/rules.d/audit.rules
+echo "-w /etc/sysconfig/network-scripts/ -p wa -k system-locale" >> /etc/audit/rules.d/audit.rules
+echo "-w /etc/group -p wa -k identity" >> /etc/audit/rules.d/audit.rules
+echo "-w /etc/passwd -p wa -k identity" >> /etc/audit/rules.d/audit.rules
+echo "-w /etc/gshadow -p wa -k identity" >> /etc/audit/rules.d/audit.rules
+echo "-w /etc/shadow -p wa -k identity" >> /etc/audit/rules.d/audit.rules
+echo "-w /etc/security/opasswd -p wa -k identity" >> /etc/audit/rules.d/audit.rules
+echo "-a always,exit -F arch=b64 -S adjtimex -S settimeofday -k time-change" >> /etc/audit/rules.d/audit.rules
+echo "-a always,exit -F arch=b32 -S adjtimex -S settimeofday -S stime -k time-change" >> /etc/audit/rules.d/audit.rules
+echo "-a always,exit -F arch=b64 -S clock_settime -k time-change" >> /etc/audit/rules.d/audit.rules
+echo "-a always,exit -F arch=b32 -S clock_settime -k time-change" >> /etc/audit/rules.d/audit.rules
+echo "-w /etc/localtime -p wa -k time-change" >> /etc/audit/rules.d/audit.rules
+echo "GRUB_CMDLINE_LINUX=\"audit=1\"" >> /etc/default/grub
+echo "GRUB_CMDLINE_LINUX=\"ipv6.disable=1\"" >> /etc/default/grub
+
+# 4.1.3 Helper
+grub2-mkconfig -o /boot/grub2/grub.cfg
